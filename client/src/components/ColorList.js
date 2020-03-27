176142d02766722168a969b95d8b3f9e6aa83ebc
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-03-27 10:47:44 -0700

    mvp refresh to see .delete

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -10,6 +10,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  // const [colorToAdd, setColorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -21,11 +22,41 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res =>{
+        console.log(res);
+        updateColors([
+          ...colors.filter(color => color.id !== colorToEdit.id),
+          res.data
+        ])
+        setEditing(false);
+      })
+      .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`api/colors/${color.id}`)
+      .then(res => {
+        updateColors(
+          colors.filter(c=> c.id !== color)
+          )
+      })
+      .catch(err => console.log(err))
   };
+
+  //  const addColor = e => {
+  //    e.preventDefault();
+  //    axiosWithAuth()
+  //     .post("api/colors", colorToAdd)
+  //     .then(res => {
+  //       console.log(res)
+  //       updateColors([...colors, colorToAdd])
+  //     })
+  //     .catch(err => console.log(err))
+  //  }
 
   return (
     <div className="colors-wrap">
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+    const [user, setUser] = useState({
+      username: "",
+      password: ""
+    })
+    const handleChange = e => {
+      setUser({
+        ...user, 
+        [e.target.name]: e.target.value
+      })
+    }
+    const handleSubmit = e => {
+      e.preventDefault();
+      axiosWithAuth()
+        .post("/api/login", user)
+        .then(res => {
+          console.log(res);
+          localStorage.setItem('token', JSON.stringify(res.data.payload));
+          props.history.push("/bubble-page")
+        })
+        .catch(err =>{
+          console.log(err)
+        })
+    }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <h4>Username</h4>
+        <input
+          type="username"
+          name="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <h4>Password</h4>
+        <input
+          type="password"
+          name="password"
+          value={user.passwword}
+          onChange={handleChange}
+        />
+        <button type="submuit">Login</button>
+      </form>
     </>
   );
 };
